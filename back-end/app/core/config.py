f8a070848ccc130ce87023586c4a1924ed088684
Seyun Sim <tlatpdbs021104@gmail.com>
2025-10-12 01:19:50 +0900

    /user/me - user api

--- back-end/app/core/config.py
+++ back-end/app/core/config.py
@@ -1,6 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    PROJECT_NAME: str = "SKKUed-IN"
+    API_V1_STR: str = "/api/v1"
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
--- back-end/app/schemas/token.py
+++ back-end/app/schemas/token.py
@@ -7,4 +7,4 @@ class Token(BaseModel):
 
 
 class TokenData(BaseModel):
-    email: str | None = None
+    sub: str | None = None
